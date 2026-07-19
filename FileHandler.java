fb5a25fa3d517a4597ed6ed085a47eed9d6abf83
PRAVENDU PRIYADARSHI BARAL <pravendu.25bce7902@vitapstudent.ac.in>
2026-07-19 13:05:29 +0530

    Add exit option to file handler menu

--- FileHandler.java
+++ FileHandler.java
@@ -4,25 +4,30 @@ public class FileHandler{
         Scanner sc=new Scanner(System.in);
         System.out.println("Welcome to the file handler.");
         System.out.println("Enter your choice");
-        System.out.println("1.Create\n2.Write into a file\n3.Extract data from a file\n4.Copy file to another location\n5.Delete the file");
-        
+         
         boolean check=true;
         while(check){
+            System.out.println("1.Create\n2.Write into a file\n3.Extract data from a file\n4.Copy file to another location\n5.Delete the file\n6.Exit");
             int ch=accept(sc);
             switch(ch){
-                case 1: CreateN.create(sc); check=false;
+                case 1: CreateN.create(sc); 
                     break;
-                case 2: WriteN.write(sc); check=false;
+                case 2: WriteN.write(sc); 
                     break;
-                case 3: ExtractN.extract(sc); check=false;
+                case 3: ExtractN.extract(sc); 
                     break;
-                case 4: TransferN.relocate(sc); check=false;
+                case 4: TransferN.relocate(sc); 
                     break;
-                case 5: DeleteN.delete(sc); check=false;
+                case 5: DeleteN.delete(sc); 
                     break;
+                case 6: System.out.println("Exiting the program...");
+                        check=false;  
+                        break;  
                 default: System.out.println("invalid choice");
             }
+            
         }
+
 
         sc.close();
     }   
